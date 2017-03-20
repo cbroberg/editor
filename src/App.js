7f1397524b1776d26e3d30ea3ff364349858c7f6
Christian Broberg <cb@webhouse.dk>
2017-03-20 12:38:27 +0100

    Preload text updated

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@
 import React from 'react'
 import TinyMCE from 'react-tinymce'
 import MyEditor from './DraftJS.js'
-
 /*eslint-enable */
 
 class App extends React.Component {
   handleEditorChange = (e) => {
-    console.log('Content was updated:', e.target.getContent());
+    console.log('Content was updated:', e.target.getContent())
   }
 
   render() {
     return (
       <div>
-        {/*<TinyMCE
-          content="<p>This is the initial content of the editor</p>"
+        <TinyMCE
+          content="<p>This is a story about a man who wanted to learn to develop code again. He started off with learning ReactJS which is just awesome :)</p>"
           config={{
             plugins: 'link image code',
             toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
           }}
           onChange={this.handleEditorChange}
-        />*/}
+        />
         <MyEditor />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
 
